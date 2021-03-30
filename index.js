ea279bf5696608bc7287dceb2877cd27f6d6fa7d
HannaBM12 <Hannabmulugeta@gmail.com>
2021-03-29 21:16:20 -0400

    commit tree 4671009f44f5ac903058c312313b8d2c3d83ceab

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(drivers){
+    return drivers.slice(0,2)
+    // for(i=0; i<drivers.length; i++)
+}
+
+const returnLastTwoDrivers = function(drivers){
+    return drivers.slice(- 2)
+}
+
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(int){
+    return function(num){
+        return num * int
+    }
+}
+
+const fareDoubler = createFareMultiplier(2)
+
+const fareTripler = createFareMultiplier(3)
+
+function selectDifferentDrivers(drivers, array2){
+    if (array2 === returnLastTwoDrivers){
+        return returnLastTwoDrivers(drivers)
+    }
+    return returnFirstTwoDrivers(drivers)
+}
